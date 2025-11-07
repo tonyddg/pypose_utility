903d79cbde849a034eebd487cbee394fed03d5bd
tonyddg <799052200@qq.com>
2025-11-07 20:58:20 +0800

    fix: 修改原有齐次矩阵变换存在的问题 feat: 重载乘法运算法, 可使用乘法组合位姿

--- src/pose_utility/__init__.py
+++ src/pose_utility/__init__.py
@@ -1,1 +1,13 @@
 from .pose_utility import Pose
+
+if __name__ == "__main__":
+    import numpy as np
+
+    T0 = Pose.from_T(np.array([
+        [-1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, -1, 0],
+        [0, 0, 0, 1]
+    ]))
+
+    print(T0 * Pose.from_xyzrxryrz_list([0, 0, 1, 0, 0, 0]))
--- src/pose_utility/pose_utility.py
+++ src/pose_utility/pose_utility.py
@@ -1,5 +1,5 @@
 import copy
-from typing import Sequence
+from typing import Optional, Sequence, Tuple
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 from dataclasses import dataclass
@@ -8,9 +8,7 @@ from dataclasses import dataclass
 class Pose:
     position: np.ndarray # 位置
     orientation: R # 姿态
-
-    def __str__(self) -> str:
-        return f"position: {str(self.position)}, euler: {str(self.orientation.as_euler('xyz'))}, uniq quat: {str(self.orientation.as_quat(True))}"
+    _T: Optional[np.ndarray] = None # 缓存的齐次矩阵, 用于姿态相关运算
 
     @classmethod
     def copy(
@@ -20,6 +18,49 @@ class Pose:
             obj.position.copy(),
             copy.copy(obj.orientation)
         )
+    def calc_T(
+        self,
+    ):
+        if self._T is None:
+            T = np.identity(4)
+            T[:3, :3] = self.orientation.as_matrix()
+            T[:3, 3] = self.position
+            self._T = T
+        
+        return self._T
+    def inv(
+        self,
+    ):
+        return self.from_T(np.linalg.inv(self.calc_T()))
+    @classmethod
+    def pose_mul(
+        cls,
+        a: "Pose",
+        b: "Pose" 
+    ):
+        return np.linalg.matmul(a.calc_T(), b.calc_T())
+    @classmethod
+    def distance(
+        cls,
+        a: "Pose",
+        b: "Pose", 
+    ) -> Tuple[float, float]:
+        '''
+        获取两个位姿的距离差 (标量单位) 与角度差 (单位 rad)
+        '''
+        diff_mat = a * b.inv()
+        pos_diff = np.linalg.norm(diff_mat.position, ord = 2)
+        rot_diff = np.linalg.norm(diff_mat.orientation.as_rotvec(degrees = False), ord = 2)
+
+        return (float(pos_diff), float(rot_diff))
+
+    def __mul__(
+        self,
+        other: "Pose"   
+    ):
+        return self.from_T(self.pose_mul(self, other))
+    def __str__(self) -> str:
+        return f"position: {str(self.position)}, euler: {str(self.orientation.as_euler('xyz'))}, uniq quat: {str(self.orientation.as_quat(True))}"
 
     @classmethod
     def from_xyzwxyz_list(
@@ -40,7 +81,7 @@ class Pose:
         xyzrxryrz_list: Sequence
     ):
         position = np.asarray(xyzrxryrz_list[:3], dtype = np.float64)
-        orientation = R.from_euler(seq = "xyz", angles = xyzrxryrz_list[3:])
+        orientation = R.from_euler(seq = "xyz", angles = np.asarray(xyzrxryrz_list[3:], dtype = np.float64))
         return Pose(position, orientation)
     def to_xyzrxryrz_list(
         self,
@@ -52,13 +93,10 @@ class Pose:
         cls,
         T: np.ndarray
     ):
-        position = np.asarray(T[3, :3], dtype = np.float64)
+        position = np.asarray(T[:3, 3], dtype = np.float64)
         orientation = R.from_matrix(T[:3, :3])
-        return Pose(position, orientation)
+        return Pose(position, orientation, _T = T)
     def to_T(
         self,
     ):
-        T = np.identity(4)
-        T[:3, :3] = self.orientation.as_matrix()
-        T[3, :3] = self.position
-        return T
+        return self.calc_T()
